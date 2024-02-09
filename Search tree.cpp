e6728d326c8eb5c162344ad990815c0ad8fddbf1
Nikita <ppdbavyss@vk.com>
2024-02-09 13:51:41 +0400

    Добавьте файлы проекта.

---
+++ Search tree.cpp
@@ -0,0 +1,18 @@
+﻿#include "Search tree.h"
+
+using namespace std;
+using namespace tree;
+
+int main(){
+
+	Node a(2);
+	Node l(1);
+	Node r(3);
+	a._left = &l;
+	a._right = &r;
+	cout << a._value << endl;
+	cout << a._left->_value << endl;
+	cout << a._right->_value << endl;
+
+	return 0;
+}
---
+++ Search tree.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include <iostream>
+
+namespace tree {
+
+	struct Node {
+		int _value;
+		Node* _left;
+		Node* _right;
+		Node(int value) :_value(value), _left(nullptr), _right(nullptr) {}
+	};
+
+	class BTree {
+	private:
+		Node* _root;
+	public:
+		BTree() : _root(nullptr) {};
+
+		BTree(Node* rootNode) : _root(rootNode) {};
+
+		void destroyTree(Node* node);
+		~BTree();
+
+		BTree& operator=(BTree lhs);
+
+		void print();
+
+		bool insert(int key);
+
+		bool contains(int key);
+
+		bool erase(int key);
+
+	};
+
+	void BTree::destroyTree(Node* node) {
+
+	}
+
+	BTree::~BTree() {
+
+	}
+
+	BTree& BTree::operator=(BTree lhs) {
+
+	}
+
+	void BTree::print() {
+
+	}
+
+	bool BTree::insert(int key) {
+		return true;
+	}
+
+	bool BTree::contains(int key) {
+		return true;
+	}
+
+	bool BTree::erase(int key) {
+		return true;
+	}
+
+}
+
