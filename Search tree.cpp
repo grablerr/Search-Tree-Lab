58d1b6be1a7207d17600de8126de379945461146
Nikita <ppdbavyss@vk.com>
2024-03-17 14:11:14 +0400

    tests

--- Search tree.cpp
+++ Search tree.cpp
@@ -1,17 +1,31 @@
 ﻿#include "Search tree.h"
+#include <set>
+#include <random>
 
 using namespace std;
 using namespace tree;
 
+void tests() {
+	BTree set;
+	set.print();
+	set.insert(0);
+	set.erase(0);
+	cout << endl;
+	set.print();
+	cout << endl;
+	for (int i = 0; i < 20; ++i) {
+		set.insert(i);
+	}
+	set.print();
+
+	for (int i = 0; i < 20; i += 2) {
+		set.erase(i);
+	}
+	if(set.insert(2))cout << '\n' << "aboba" << '\n';
+	set.print();
+}
+
 int main(){
-
-	BTree MyTree;
-	MyTree.insert(8);
-	MyTree.insert(6);
-	MyTree.insert(4);
-	MyTree.insert(9);
-
-	MyTree.print();
-
+	tests();
 	return 0;
 }
